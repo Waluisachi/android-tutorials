ef066805fd96786b0a7411691476a5ee5cab358b
philipwaluisachi <philipwaluisachi@gmail.com>
2021-11-04 00:10:50 -0400

    Second activity added

--- app/src/main/java/com/waluisachi/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/waluisachi/msgshareapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.waluisachi.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.btnShowToast)
+        val button2: Button = findViewById(R.id.btnSendMsgToNextActivity)
+        val et: EditText = findViewById(R.id.etUserMessage)
 
         button.setOnClickListener {
             Log.i("MainActivity", "Button clicked")
 
             Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        button2.setOnClickListener {
+            val message: String = et.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT)
+            //Navigate to a new activity(Screen)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
